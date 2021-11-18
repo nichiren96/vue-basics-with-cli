29071cfcb8469216dee13a341813d1f00b72d900
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-18 19:37:04 +0300

    Using refs

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>Hello Vue CLI</h1>
+  <h1>Hello Vue CLI</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
 export default {
-   
-}
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
 </script>
