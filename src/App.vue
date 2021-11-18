865784930085e051282233fe404d609ed5dcc9c3
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-18 19:57:47 +0300

    Using scoped styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <h1>Root Component</h1>
   <Modal />
 </template>
 
@@ -22,5 +23,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
